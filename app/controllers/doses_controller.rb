46b5eeef50270008c71ce21ca29e83099e677e84
NedijkoSipic <nesipic@hotmail.com>
2020-11-12 15:50:53 +0100

    Core part done, works fine, rake 100% green, style need much work

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,31 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
   def new
+    @dose = Dose.new
   end
 
   def create
+  @dose = Dose.new(dose_params)
+  @dose.cocktail = @cocktail
+  #@dose.ingredient = Ingredient.find_by_name(params[:ingredient])
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find_by_id(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true, length: { minimum: 1 }
-  validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
 end
